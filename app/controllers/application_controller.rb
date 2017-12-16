8a0f1e47463f3351803b3b7fac37123e657ad22d
veloblank <veloblank@gmail.com>
2017-12-16 02:05:05 -0500

    Create main index and build out helper frame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :sessions, true
   end
+
+
+  get '/' do
+    erb :index
+  end
+
+helpers do
+
+  def current_user
+
+  end
+
+
+  def logged_in?
+
+  end
+
+
+end
 
 
 end
