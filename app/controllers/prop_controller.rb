ddb71bc069543e6dad5368fa03a41dfb1bb89a40
veloblank <veloblank@gmail.com>
2017-12-16 07:06:58 -0500

    Create 'scrape' route

--- app/controllers/prop_controller.rb
+++ app/controllers/prop_controller.rb
@@ -1,10 +1,18 @@
 class PropController < ApplicationController
 
   get '/props/index' do
-    Scraper.scrape_props
-    @props = Prop.all_props
     erb :"props/index"
   end
+
+  get '/scrape' do
+    Scraper.scrape_props
+    @props = Prop.all_props
+    erb :"/props/index"
+
+  end
+
+
+
 
 
 end
