ceee7ac86eed78e61d0151f05faa5d2d3c1a5655
veloblank <veloblank@gmail.com>
2017-12-16 08:41:11 -0500

    Remove unneeded prop attributes

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,22 +1,18 @@
 class Prop < ActiveRecord::Base
 
-  #@@scraped_prop_data = []
   @@props = []
 
-  attr_accessor :prop_id_num, :event_title, :start_time, :sport, :away_team, :home_team, :prop_preview, :away_team_url, :home_team_url, :matchup_status, :automate_pick_time
+  attr_accessor :event_title, :start_time, :sport, :away_team, :home_team, :away_team_url, :home_team_url, :matchup_status
 
   def initialize(hash)
-    @prop_id_num = hash[:prop_id_num]
     @event_title = hash[:event_title]
     @start_time = hash[:start_time]
     @sport = hash[:sport]
     @away_team = hash[:away_team]
     @home_team = hash[:home_team]
-    @prop_preview = hash[:prop_preview]
     @away_team_url = hash[:away_team_url]
     @home_team_url = hash[:home_team_url]
     @matchup_status = hash[:matchup_status]
-    @automate_pick_time = hash[:automate_pick_time]
     @@props << self
   end
 
