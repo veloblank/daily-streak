1a334f8e06ef20bff8ba745310f99352f7157dcb
veloblank <veloblank@gmail.com>
2017-12-16 03:39:05 -0500

    Use AR validations on new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  has_secure_password
 end
